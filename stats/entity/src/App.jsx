e7dde7cb8ca4bd993b1172d2adc3b410a27a11ea
Rainer Simon <rainer.simon@ait.ac.at>
2019-07-29 09:16:05 +0200

    Entity stats page setup

--- stats/entity/src/App.jsx
+++ stats/entity/src/App.jsx
@@ -1,36 +1,27 @@
 import React, { Component } from 'react';
-import 'chart.js';
-import AnnotationStats from 'common/AnnotationStats';
+import FlaggedEntities from './flagged/FlaggedEntites';
+import axios from 'axios';
 
 import './App.scss';
-
-const COLORS = [
-  '#57c17b',
-  '#6f87d8',
-  '#bc51bc',
-  '#afafaf'
-];
 
 export default class App extends Component {
   
   state = {
-    document: this.props.config.document || 'asckvtl43e9jca',
-    annotations: [],
-    editsPerUser: [], 
-    activityPerUser: []
+    document: this.props.config.document || 'oflmsfz9augu6l',
+    annotations: []
+  }
+
+  componentDidMount() {
+    axios.get(`/api/document/${this.state.document}/annotations`)
+      .then(response => this.setState({ annotations: response.data }));
   }
 
   render() {
-    const stats = new AnnotationStats(this.state.annotations);
-
     return (
-      <div>
-        <div className="panel">
-          <h2>Top Places</h2>
-          <div className="inner">
-          </div>
-        </div>
-      </div>
+      <>
+        <FlaggedEntities
+            annotations={this.state.annotations} />
+      </>
     )
   }
 
---
+++ stats/entity/src/flagged/FlaggedEntites.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FlaggedEntites = props => {
+
+  return (
+    <div>
+      <div className="panel w12">
+        <h2>Flagged Toponyms</h2>
+        <div className="inner">
+
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default FlaggedEntites;
